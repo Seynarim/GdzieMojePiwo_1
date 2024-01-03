63bd8f08f923a0afcee6f485468275919eb363be
Seynarim <wojtekvicta@gmail.com>
2024-01-03 19:12:59 +0100

    0.14A
    test

--- resources/views/Home.blade.php
+++ resources/views/Home.blade.php
@@ -6,22 +6,7 @@
 
 <h1>home</h1>
 
-<!-- Message if edit succed -->
-<div>
-    @if(session()->has('success'))
-        <div>
-            {{session('success')}}
-        </div>
-    @endif
-</div>
-
-<!-- CREATE A BEER BUTTON -->
-<button  class="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
-  <a href = "{{route('pubs.create')}}">
-    Create a Beer
-    </a>
-</button>
-
+Welcome to the Jungle
 
 
 @stop
