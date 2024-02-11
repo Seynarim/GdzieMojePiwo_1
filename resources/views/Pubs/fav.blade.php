58a562c47f21ddcc36819f2e179e9fa29cbac7d5
Seynarim <wojtekvicta@gmail.com>
2024-02-11 16:20:11 +0100

    1.29B Fix

--- resources/views/Pubs/fav.blade.php
+++ resources/views/Pubs/fav.blade.php
@@ -14,15 +14,7 @@
         <!-- Napis wyniki wyszukiwania -->
         <h1 class="text-xl font-semibold tracking-tight">Puby</h1>
         <!-- Ikona ustawień -->
-<<<<<<< Updated upstream
-        <a href="#" class="text-3xl">
-            <svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
-                <path d="M1 5h1.424a3.228 3.228 0 0 0 6.152 0H19a1 1 0 1 0 0-2H8.576a3.228 3.228 0 0 0-6.152 0H1a1 1 0 1 0 0 2Zm18 4h-1.424a3.228 3.228 0 0 0-6.152 0H1a1 1 0 1 0 0 2h10.424a3.228 3.228 0 0 0 6.152 0H19a1 1 0 0 0 0-2Zm0 6H8.576a3.228 3.228 0 0 0-6.152 0H1a1 1 0 0 0 0 2h1.424a3.228 3.228 0 0 0 6.152 0H19a1 1 0 0 0 0-2Z"/>
-            </svg>
-        </a>
-=======
         @include('elements.log_icon')
->>>>>>> Stashed changes
     </div>
 
     <!-- Okno wyszukiwania -->
